d491294cf12835908c6c9e93be46f2cc128bc747
Nabajyot Khamari <35532208+codewithNabajyot@users.noreply.github.com>
2022-07-16 10:39:43 +0530

    Splash component

---
+++ src/components/Splash/Splash.js
@@ -0,0 +1,9 @@
+import "./splash.css";
+export default function Splash() {
+  return (
+    <>
+      <span id="splash-overlay" className="splash"></span>
+      <span id="welcome"></span>
+    </>
+  );
+}
